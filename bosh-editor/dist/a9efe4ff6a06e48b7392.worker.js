28eddc6781a41696d1beab87e270ba13c4bc2ad0
Jamil Shamy <jamilshamy@gmail.com>
2016-11-20 21:18:20 -0500

    Push bosh-editor beta

---
+++ bosh-editor/dist/a9efe4ff6a06e48b7392.worker.js
@@ -0,0 +1,2 @@
+!function(e){function r(t){if(n[t])return n[t].exports;var o=n[t]={exports:{},id:t,loaded:!1};return e[t].call(o.exports,o,o.exports,r),o.loaded=!0,o.exports}var n={};return r.m=e,r.c=n,r.p="dist/",r(0)}([function(e,r){onmessage=function(e){postMessage({errors:[],specs:e.data.definition,warnings:[]})}}]);
+//# sourceMappingURL=a9efe4ff6a06e48b7392.worker.js.map
